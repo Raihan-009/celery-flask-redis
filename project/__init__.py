8bf3ddf4c1b36aa157ce390c4659a98f24e96b46
Raihan Islam <u1809009@student.cuet.ac.bd>
2023-10-04 16:27:12 +0600

    scheduled task is working

--- project/__init__.py
+++ project/__init__.py
@@ -6,7 +6,13 @@ def create_app():
     app = Flask(__name__)
     app.config["CELERY_CONFIG"] = {
         "broker_url": "redis://celery-redis:6379/0", 
-        "result_backend": "redis://celery-redis:6379/0", 
+        "result_backend": "redis://celery-redis:6379/0",
+        "beat_schedule": {
+            "scheduled-task":{
+                "task" : "project.tasks.scheduled_task",
+                "schedule" : 15
+            }
+        } 
     }
 
     celery = make_celery(app)
--- project/tasks.py
+++ project/tasks.py
@@ -10,3 +10,11 @@ def add(x, y):
 @shared_task
 def sub(x, y):
     return x - y
+
+@shared_task
+def mul(x, y):
+    return x * y
+
+@shared_task
+def scheduled_task():
+    print("Periodic task is happening")
